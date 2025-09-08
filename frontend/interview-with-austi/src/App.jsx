64433e5a693cf268f4d82414f08c096d05bf04bc
austinxduong <austinxduong@gmail.com>
2025-09-07 21:07:38 -0700

    refactor file structure

--- frontend/interview-with-austi/src/App.jsx
+++ frontend/interview-with-austi/src/App.jsx
@@ -1,4 +1,13 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import Login from "./pages/Auth/Login"
+import SignUp from './pages/Auth/SignUp';
+import LandingPage from "./pages/LandingPage";
+import Dashboard from "./pages/Home/Dashboard";
+import InterviewPrep from "./pages/InterviewPrep/InterviewPrep"
+
 
 const App = () => {
   return (
