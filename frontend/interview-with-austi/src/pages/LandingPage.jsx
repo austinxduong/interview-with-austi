54a94f7f557535c4858fab14c1ecf9332d752501
austinxduong <austinxduong@gmail.com>
2025-09-08 18:24:49 -0700

    define routes

--- frontend/interview-with-austi/src/pages/LandingPage.jsx
+++ frontend/interview-with-austi/src/pages/LandingPage.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+// import HERO_IMG from "../assets/hero-img.png";
+// import { APP_FEATURES } from "../utils/data"
+
 const LandingPage = () => {
     return (
         <div>LandingPage</div>
